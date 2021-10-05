0ae46061c76366c7f2a08304e7ebf0b89b9dd3ff
Alexlife9 <crashmx@inbox.ru>
2021-10-05 15:38:53 +0300

    3. Демонстрация добавления и вывода элементов в различных реализациях Set

---
+++ src/main/java/ru/alex_life/collections/UseSet.java
@@ -0,0 +1,45 @@
+package ru.alex_life.collections;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Наглядная демонстрация добавления и вывода элементов в различных реализациях Set
+ * !Удаляются дубликаты!
+ *
+ * @author Alex_life
+ * @version 1.0
+ * @since 29.09.2021
+ */
+public class UseSet {
+    public static void main(String[] args) {
+        System.out.println("HashSet: хаотичный порядок элементов");
+        Set<String> hashSet = new HashSet<>();
+        hashSet.add("яблоко");
+        hashSet.add("ананас");
+        hashSet.add("банан");
+        hashSet.add("яблоко");
+        System.out.println(hashSet);
+        System.out.println();
+
+        System.out.println("LinkedHashSet: в порядке добавления");
+        Set<String>  linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add("яблоко");
+        linkedHashSet.add("яблоко");
+        linkedHashSet.add("ананас");
+        linkedHashSet.add("банан");
+        System.out.println(linkedHashSet);
+        System.out.println();
+
+        System.out.println("TreeSet: красно-черное дереве, сортировка по алфавиту");
+        Set<String>  treeSet = new TreeSet<>();
+        treeSet.add("яблоко");
+        treeSet.add("яблоко");
+        treeSet.add("ананас");
+        treeSet.add("банан");
+        System.out.println(treeSet);
+        System.out.println();
+    }
+}
