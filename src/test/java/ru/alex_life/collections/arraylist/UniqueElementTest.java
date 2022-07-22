76b8b238843fe5938b4ccdf14f1144bef72a4094
Alexlife9 <crashmx@inbox.ru>
2022-07-23 02:32:56 +0300

    81. Упражнения. Найти индекс элемента, чтобы понять уникальный ли элемент в списке

---
+++ src/test/java/ru/alex_life/collections/arraylist/UniqueElementTest.java
@@ -0,0 +1,49 @@
+package ru.alex_life.collections.arraylist;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Упражнения. Найти индекс элемента
+ *
+ * @author Alex_life
+ * @version 1.0
+ * @since 23.07.2022
+ */
+public class UniqueElementTest {
+    @Test
+    public void checkListTrue() {
+        List<String> list = new ArrayList<>();
+        list.add("first");
+        list.add("second");
+        list.add("third");
+        boolean result = UniqueElement.checkList(list, "second");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void checkListFalse() {
+        List<String> list = new ArrayList<>();
+        list.add("first");
+        list.add("second");
+        list.add("third");
+        list.add("second");
+        list.add("fourth");
+        boolean result = UniqueElement.checkList(list, "second");
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void checkListNoContainsElement() {
+        List<String> list = new ArrayList<>();
+        list.add("first");
+        list.add("second");
+        list.add("third");
+        boolean result = UniqueElement.checkList(list, "fourth");
+        assertThat(result, is(false));
+    }
+}
