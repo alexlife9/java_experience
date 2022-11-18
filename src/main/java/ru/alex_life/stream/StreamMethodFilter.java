d1e5e37b9428b853557f068cca6a3d8ecf89df45
Alexlife9 <crashmx@inbox.ru>
2022-11-19 00:46:29 +0300

    135. Видео-курс Черный пояс. Streams. Метод filter

---
+++ src/main/java/ru/alex_life/stream/StreamMethodFilter.java
@@ -0,0 +1,106 @@
+package ru.alex_life.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Видео-курс Черный пояс
+ * Streams
+ * Метод filter
+ *
+ * Фильтрует элементы потока, возвращая только элементы, удовлетворяющие условию.
+ *
+ * @author Alex_life
+ * @version 1.0
+ * @since 19.11.2022
+ */
+public class StreamMethodFilter {
+    public static void main(String[] args) {
+        Student student1 = new Student("Ivan", 'm', 22, 3, 8.3);
+        Student student2 = new Student("Niko", 'm', 28, 2, 6.4);
+        Student student3 = new Student("Elen", 'f', 19, 1, 8.9);
+        Student student4 = new Student("Petr", 'm', 35, 4, 7);
+        Student student5 = new Student("Mary", 'f', 23, 3, 9.1);
+
+        List<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+
+        students = students.stream()
+                .filter(element ->
+                        element.getAge() > 22 && element.getAvgGrade() < 7.2)
+                .collect(Collectors.toList());
+        System.out.println(students);
+    }
+}
+
+class Student {
+    private String name;
+    private char sex;
+    private int age;
+    private int course;
+    private double avgGrade;
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public double getAvgGrade() {
+        return avgGrade;
+    }
+
+    public void setAvgGrade(double avgGrade) {
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+}
